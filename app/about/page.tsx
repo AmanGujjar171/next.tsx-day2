d4d04b777fe3b1627274009b4c13ef1c4b649140
AMANGUJJAR <ag3734359@gmail.com>
2024-06-04 23:29:14 +0500

    day2

---
+++ app/about/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+       <section className="Sub-header">
+        <nav>
+            <a href="index.html" className="logo">Xplore
+                <i className="fab fa-staylinked"></i>kill
+            </a>
+            <div className="nav-links" id="navLinks">
+                {/* <!-- reposnive bar open and close --> */}
+                <i className="fa fa-times" ></i>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="course.html">Course</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <i className="fa fa-bars"></i>
+            {/* <!-- reposnive bar open and close --> */}
+        </nav>
+        <h1>About Us</h1>
+    </section>
+
+    {/* <!-- About Us Section Start --> */}
+
+    <section className="about-us">
+        <div className="row">
+            <div className="about-col">
+                <h1>We are the world's largest University</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
+                <a href="#" className="hero_btn btn">EXPLORE NOW</a>
+            </div>
+            <div className="about-col">
+                <img src="img/about.png" alt="" />
+            </div>
+        </div>
+    </section>
+
+    {/* <!-- About Us Section end --> */}
+
+
+  
+    </div>
+  )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <section className="Sub-header">
+        <nav>
+          <a href="index.html" className="logo">Xplore
+            <i className="fab fa-staylinked"></i>kill
+          </a>
+          <div className="nav-links" id="navLinks">
+
+            {/* <!-- reposnive bar open and close --> */}
+            <i className="fa fa-times" ></i>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="course.html">Course</a></li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </div>
+          <i className="fa fa-bars"></i>
+
+          {/* <!-- reposnive bar open and close --> */}
+
+        </nav>
+        <h1>Contact Us</h1>
+      </section>
+
+      {/* <!-- Contact Us Section Start --> */}
+
+      <section className="loacation">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111989.31313237658!2d77.32498792347556!3d28.699635117457326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf1bb41c50fdf%3A0xe6f06fd26a7798ba!2sGhaziabad%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1630495771496!5m2!1sen!2sin"
+          width="600" height="450" loading="lazy"></iframe>
+      </section>
+
+      <section className="contact-us">
+        <div className="row">
+          <div className="content-col">
+            <div>
+              <i className="fa fa-home"></i>
+              <span>
+                <h5>NH9 Road, ABC Building</h5>
+                <p>Ghaziabad, Uttar Pradesh, IN</p>
+              </span>
+            </div>
+            <div>
+              <i className="fa fa-phone"></i>
+              <span>
+                <h5>+91 7445546525</h5>
+                <p>Monday To Saturday, 9AM To 6PM</p>
+              </span>
+            </div>
+            <div>
+              <i className="fa fa-envelope"></i>
+              <span>
+                <h5>xyz@email.com</h5>
+                <p>Email Us Yor Query</p>
+              </span>
+            </div>
+          </div>
+          <div className="content-col">
+            <form>
+              <input type="text" placeholder="Enter Name" required />
+              <input type="email" placeholder="Enter Email" required />
+              <input type="text" placeholder="Enter Subject" required />
+              <textarea placeholder="Message" required></textarea>
+              <button type="submit" className="hero_btn btn">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Contact Us Section End --> */}
+    </div>
+      
+        )
+}
