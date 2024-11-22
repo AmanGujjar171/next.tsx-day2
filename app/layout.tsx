3eea0aa7ae8d1ed4a5f926accf9aca6cdeb05a9f
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-22 18:01:50 +0500

    wroking

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        {/* <Navbar /> */}
         {children}
         <Footer />
       </body>
